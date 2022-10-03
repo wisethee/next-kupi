5dccf5142bb9510d6466b48a4a2baadf5ead21e6
marius <marius.paduraru@gmail.com>
2022-10-03 19:59:46 +0100

    build: remove nx component, add favicon

--- apps/kupi/pages/_app.tsx
+++ apps/kupi/pages/_app.tsx
@@ -1,12 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Welcome to kupi!</title>
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
       </Head>
       <main className="app">
         <Component {...pageProps} />
